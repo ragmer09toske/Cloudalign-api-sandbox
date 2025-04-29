4c226631f7d514d40ceda8b4068156d65a452ab3
Retsepile Shao <73499664+ragmer09toske@users.noreply.github.com>
2025-04-29 21:12:36 +0200

    fix: restrict CORS origin to localhost for improved security

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["*"],
+    origin: ["http:/localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
